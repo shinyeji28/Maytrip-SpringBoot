a67ff44c1c36d8f3756da9314172390e32553f4b
miraekwak <alfo9490@naver.com>
2023-11-07 08:51:33 +0900

    initial project

---
+++ src/main/java/com/ssafy/maytrip/MaytripApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.maytrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaytripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaytripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssafy/maytrip/MaytripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.maytrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaytripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
